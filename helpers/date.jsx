4aadfebc9eb14a01158151838a08d9a92b823316
Oliver <oliverjohnpr2013@gmail.com>
2024-09-26 09:31:06 +0800

    More cosmetic changes

--- helpers/date.jsx
+++ helpers/date.jsx
@@ -20,3 +20,30 @@ export const formatMessageDateShort = (date) => {
         return format(inputDate, 'dd/MM/yyyy'); // e.g., "03/09/2023"
     }
 };
+
+export const formatMessageDateLong = (date) => {
+    const now = new Date();
+    const inputDate = new Date(date);
+
+    if(isToday(inputDate)) {
+        return inputDate.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+    } else if (isYesterday(inputDate)) {
+        return (
+            "Yesterday " +
+            inputDate.toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+            })
+        )
+    } else if (inputDate.getFullYear() === now.getFullYear()) {
+        return inputDate.toLocaleDateString([], {
+            day: "2-digit",
+            month: "short",
+        });
+    } else {
+        return inputDate.toLocaleDateString();
+    }
+}
